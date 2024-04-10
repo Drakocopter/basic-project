e1dad2ef0bfe0aebab2904568910332439ed7fab
Drakocopter <lightningstaff44@gmail.com>
2024-04-10 14:29:07 -0400

    Added Links, Styling, Images and some Javascript

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+// const extra_special = document.getElementById("extra_special")
+    //DOM - document object model
+extra_special.innerHTML = "Replacement Text";
+extra_special.style.color = "red"
+
+}); //END DOM CONTENT LOADED
